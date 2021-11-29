1fa73b6c4527d36654040b133c947b8c3c10b995
Rajat Rajput <rajputrajat@gmail.com>
2021-11-29 12:46:40 +0530

    not able to stdout sizeof(). build failing.

--- main.cpp
+++ main.cpp
@@ -13,13 +13,23 @@ public:
     }
 };
 
+const auto del_fn(Data<int>* p_data) {
+    std::cout << "calling custom deleter function" << std::endl;
+    delete p_data;
+}
+
 int main() {
-    auto del = [](Data<int>* p_data) {
-        std::cout << "calling custom deleter" << std::endl;
+    const auto del = [](Data<int>* p_data) {
+        std::cout << "calling custom deleter lambda" << std::endl;
         delete p_data;
     };
 
-    std::cout << "size of custom deleter 'del': " << sizeof(del) << std::endl;
+    std::cout << "size of custom deleter lambda 'del': " << sizeof(del) << std::endl;
+    std::cout << "size of custom deleter function pointer 'del_fn': " << sizeof(&del_fn) << std::endl;
 
-    auto data = std::unique_ptr<Data<int>, decltype(del)>(new Data<int>(10), del);
+    const auto data = std::unique_ptr<Data<int>, decltype(del)>(new Data<int>(10), del);
+    const auto data_fp = std::unique_ptr<Data<int>, void(*)(Data<int> *)>(new Data<int>(20), &del_fn);
+
+    std::cout << "size of unique_ptr with lambda custom-deleter: " << sizeof(data) << std::end;
+    std::cout << "size of unique_ptr with function-ptr custom-deleter: " << sizeof(data_fp) << std::end;
 }
